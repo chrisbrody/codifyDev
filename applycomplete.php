936e28ff9d0c4a8068b5ebed449e5dfda917ac7f
Chris Brody <chrisbrody@codifyacademy.com>
2015-07-24 16:09:57 -0400

    Responsive Design Complete

---
+++ applycomplete.php
@@ -0,0 +1,93 @@
+<?php  include('head.php');  ?> 
+  
+<body>
+<?php  include('header.php');  ?> 
+
+<div class="container">
+  <div class="row text-center">
+    <div class="applied rowwidth">
+      <br>
+      <h1>Application Submitted!</h1>
+      <br>
+      <img src="demos/success.jpg">
+      <br>
+      <h3>Be Talking With You Shortly</h3>
+      <br>
+    </div>
+  </div>
+</div>
+
+ <div class="copyrights">
+        <div class="container">
+            <div class="col-lg-6 col-md-6 col-sm-12 columns">
+                <p>Copyright © <script>document.write(new Date().getFullYear());</script> - All rights reserved <a title="Codify Academy" href="http://codifyacademy.com">Codify Academy</a></p>
+            </div><!-- end widget -->
+            <div class="col-lg-6 col-md-6 col-sm-12 columns text-right">
+                <div class="footer-menu right">
+                    <ul class="menu">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="thecourse.php">The Course</a></li>
+                        <li><a href="tuition.php">Tuition</a></li>
+                        <li><a href="apply.php">Apply</a></li>
+                        <li><a href="faq.php">Faq</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div><!-- end large-6 -->   
+        </div><!-- end container -->
+    </div><!-- end copyrights -->
+  <!-- Google Code for Application Submission Conversion Page -->
+  <script type="text/javascript">
+    /* <![CDATA[ */
+    var google_conversion_id = 909855431;
+    var google_conversion_language = "en";
+    var google_conversion_format = "2";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "2XPUCMnEoFkQx5XtsQM";
+    var google_remarketing_only = false;
+    /* ]]> */
+  </script>
+  <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+  </script>
+  <noscript>
+    <div style="display:inline;">
+      <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/909855431/?label=2XPUCMnEoFkQx5XtsQM&amp;guid=ON&amp;script=0"/>
+    </div>
+  </noscript>
+  <!-- Facebook Conversion Code for PE - Application Submission -->
+  <script>
+    (function() {
+      var _fbq = window._fbq || (window._fbq = []);
+      if (!_fbq.loaded) {
+        var fbds = document.createElement('script');
+        fbds.async = true;
+        fbds.src = '//connect.facebook.net/en_US/fbds.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(fbds, s);
+        _fbq.loaded = true;
+      }
+    })();
+    window._fbq = window._fbq || [];
+    window._fbq.push(['track', '6021810669217', {'value':'0.00','currency':'USD'}]);
+  </script>
+  <noscript>
+    <img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6021810669217&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1" />
+  </noscript>
+   <!-- Main Scripts-->
+<script src="assets/js/jquery.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.unveilEffects.js"></script> 
+<script src="js/retina-1.1.0.js"></script>
+<script src="js/jquery.hoverdir.js"></script>
+  <script src="js/owl.carousel.js"></script>  
+  <script src="js/jetmenu.js"></script> 
+<script src="js/jquery.hoverex.min.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/custom.js"></script>
+<!-- LayerSlider script files -->
+<script src="layerslider/js/greensock.js" type="text/javascript"></script>
+<script src="layerslider/js/layerslider.transitions.js" type="text/javascript"></script>
+<script src="layerslider/js/layerslider.kreaturamedia.jquery.js" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ faq.php
@@ -0,0 +1,284 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Codify Academy</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <link rel="shortcut icon" href="http://www.codifyacademy.com/favicon.ico?v=2" />
+    
+  <!-- Google Fonts -->
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,900,300,700' rel='stylesheet' type='text/css'> 
+  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="css/font-awesome.css">
+  <!-- Bootstrap -->
+  <link href="assets/css/bootstrap.css" rel="stylesheet">\
+  <!-- Animate Css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Template Styles -->
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/colors/turquoise.css">    
+  <!-- Layer Slider -->
+  <link rel="stylesheet" href="layerslider/css/layerslider.css" type="text/css">
+  <style type="text/css">
+    .panel-body {
+      background-color: #fff;
+    }
+    .panel-body ul {
+      color: black;
+    }
+    h4.text-center {
+      font-size:36px;
+    }
+    .panel-title a {
+      font-size: 20px;;
+    }
+  </style>
+  
+  <!-- Support for HTML5 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- Enable media queries on older browsers -->
+  <!--[if lt IE 9]>
+    <script src="assets/js/respond.min.js"></script>
+  <![endif]-->
+  <script src="js/phone.js"></script>
+  <script src="js/modernizr.js"></script>
+  <!-- KISSmetrics tracking snippet -->
+<script type="text/javascript">var _kmq = _kmq || [];
+var _kmk = _kmk || '9c27c877479ebee6d9bdc811c10d0918d4dda8c0';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+</script>
+    <!-- google analytics -->
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-46682051-2', 'auto');
+  ga('send', 'pageview');
+</script>
+  </head>
+<!--   <div class="topbar clearfix">
+    <div class="container">
+    <div class="col-lg-12 text-right">
+              <div class="social_buttons">
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Google+"><i class="fa fa-google-plus"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="Dribbble"><i class="fa fa-dribbble"></i></a>
+                  <a href="#" data-toggle="tooltip" data-placement="bottom" title="RSS"><i class="fa fa-rss"></i></a>
+              </div>
+    </div>
+    </div>
+  </div> -->
+<body>
+  <?php  include('header.php');  ?> 
+
+  <!-- social media link -->
+  <?php  include('social.php');  ?> 
+
+  <div class="jumbotron jumbotron-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-lg-12">
+                <h1 class="h1 h1course">Frequently Asked Questions</h1>
+            </div>
+        </div>
+    </div>
+  </div>
+
+<!--   <div class=" row rowwidth">
+    <div class="col-lg-2 col-lg-offset-1">
+        <div class="circleimage">
+          <img src="demos/sam.jpg" class="img-circle" style="height:116px" "width:116px"/>
+        </div>
+    </div>
+    <div class="col-lg-9">
+      <h2>Have any other questions?<h2>
+      <h3>Call Sam, <a href="tel:5185214189">(518) 521-4189</a></h3>
+    </div>
+  </div>
+  <br>
+  <hr> -->
+
+  <div class="container section-grey">
+    <div class="row rowwidth">
+      <div class="col-lg-12 columns font-size-150-percent">
+
+        <h4 class="text-center"><strong>Requirements</strong></h4>
+
+        <br>
+        <div class="panel-group" id="accordion">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                  Do I need a technical background to take the course?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse in">
+              <div class="panel-body">
+                No, there are no technical qualifications. As long as you know what a computer is, you're qualified to take our course.
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                  What do you look for in a new Student?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+              <div class="panel-body">
+                  Someone with the drive to better themselves on a daily basis.
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+                  Do I need my own laptop?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+              <div class="panel-body">
+                Yes, you'll need to bring your own laptop to class. You don't need the best laptop to work on front-end development, but you'll need to be able to access the internet at a reasonable speed.
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br>
+
+        <h4 class="text-center"><strong>The Admissions Process</strong></h4>
+        <div class="panel-group" id="accordion2">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion2" href="#collapseFour">
+                  How can I apply?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseFour" class="panel-collapse collapse in">
+              <div class="panel-body">
+                All you have to do is <a href="apply.php" title="Apply to Codify Academy" >Apply </a> and choose which class dates works for you. We'll call you and ask a few questions to get started.
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion2" href="#collapseSix">
+                  How often does a new class start?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseSix" class="panel-collapse collapse">
+              <div class="panel-body">
+                A new class starts every four weeks in each city.
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion2" href="#collapseSeven">
+                  How soon should I apply to get into a particular class?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseSeven" class="panel-collapse collapse">
+              <div class="panel-body">
+                We accept applicants on a first come first serve basis, so the sooner you apply, the more likely you are to get accepted into an earlier class.
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br>
+
+        <h4 class="text-center"><strong>Tuition</strong></h4>
+
+        <div class="panel-group" id="accordion4">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion4" href="#collapseNine">
+                  How much is tuition?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseNine" class="panel-collapse collapse in">
+              <div class="panel-body">
+                Tuition is currently $5000.
+              </div>
+            </div>
+          </div>          
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion4" href="#collapseEleven">
+                  Do you have payment plans?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseEleven" class="panel-collapse collapse">
+              <div class="panel-body">
+                We no longer offer payment plans. We recently partnered with <a href="https://lendlayer.com/" target="_blank">LendLayer</a> to provide a financing options
+                <br>
+                <br>
+                This option requires a downpayemt and 12 monthly payments <a href="tuition.php">More Details on Financing</a>
+
+              </div>
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h4 class="panel-title">
+                <a data-toggle="collapse" data-parent="#accordion4" href="#collapseTen">
+                  Do you offer other financing?
+                </a>
+              </h4>
+            </div>
+            <div id="collapseTen" class="panel-collapse collapse">
+              <div class="panel-body">
+                We have recently partnered with <a href="https://lendlayer.com/" target="_blank">LendLayer</a> to provide <a href="tuition.php">financing</a> to our students!
+              </div>
+            </div>
+          </div>
+        </div>
+
+     
+        <br>
+        <br>
+    </div>
+  </div>
+  </div>
+
+  <?php include('footer.php');?>
+    
+</body>
+</html>
